0fb034cc820e1dfaed76d62192386f9b22c1b675
radenkosetka7 <58879645+radenkosetka7@users.noreply.github.com>
2022-11-09 12:12:03 +0100

    Program craches

--- src/main/java/com/example/bldonate/services/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.bldonate.services;
-
-import com.example.bldonate.models.dto.LoginResponse;
-import com.example.bldonate.models.requests.LoginRequest;
-
-public interface AuthService {
-
-    LoginResponse login(LoginRequest request);
-}
