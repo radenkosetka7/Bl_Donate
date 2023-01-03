cfbebdc959c7536edc853d0a8908505912a60ee1
panicsanja <njasapanic@gmail.com>
2023-01-03 02:10:54 +0100

    Update RezervacijaStavkaRepository.java

--- src/main/java/com/example/bldonate/repositories/RezervacijaStavkaRepository.java
+++ src/main/java/com/example/bldonate/repositories/RezervacijaStavkaRepository.java
@@ -4,6 +4,7 @@ import com.example.bldonate.models.entities.RezervacijaEntity;
 import com.example.bldonate.models.entities.RezervacijaStavkaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,7 +12,6 @@ import java.util.List;
 public interface RezervacijaStavkaRepository extends JpaRepository<RezervacijaStavkaEntity,Integer> {
 
 
-    @Query("SELECT r from RezervacijaStavkaEntity r where r.donacijaStavka.donacija.korisnik.id=:id and " +
-            "r.donacijaStavka.donacija.arhivirana=:false")
+    @Query("SELECT r from RezervacijaStavkaEntity r where r.donacijaStavka.donacija.korisnik.id=:id and r.donacijaStavka.donacija.arhivirana=false")
     List<RezervacijaStavkaEntity> getAllReservationsByDonor(Integer id);
 }
